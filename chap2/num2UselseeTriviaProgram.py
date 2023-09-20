59bbb610eabf6531e29d199d32849afb42219635
HohoHoui <wwsddrf15102@gmail.com>
2023-09-20 12:03:10 +0900

    s

---
+++ chap2/num2UselseeTriviaProgram.py
@@ -0,0 +1,7 @@
+name = input("Hi. What's your name? ")
+age = input("And how old are you? ")
+weight = input("Okay, last question. How many kg do you weigh? ")
+
+dogAge = int(age) / 7
+print("Did you know that you're just " +dogAge + " in dog years?\n")
+print("But you're also over ")
