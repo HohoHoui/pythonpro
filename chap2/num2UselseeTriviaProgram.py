a117d0e667ca3e29e5cff0aa5ca91d9b4dbfa7cd
HohoHoui <wwsddrf15102@gmail.com>
2023-09-20 22:42:20 +0900

    practice 2 upload

--- chap2/num2UselseeTriviaProgram.py
+++ chap2/num2UselseeTriviaProgram.py
@@ -1,7 +1,17 @@
 name = input("Hi. What's your name? ")
 age = input("And how old are you? ")
-weight = input("Okay, last question. How many kg do you weigh? ")
+weigh = input("Okay, last question. How many kg do you weigh? ")
 
 dogAge = int(age) / 7
-print("Did you know that you're just " +dogAge + " in dog years?\n")
-print("But you're also over ")
+age_sec = int(age) * 365 * 24* 60 * 60
+
+print("\nDid you know that you're just %d in dog years?\n" % dogAge)
+print("But you're also over %d seconds old.\n" % age_sec)
+print("If a small child weere trying to get your attention, your name wouldbecome:")
+print("%s" % name*5)
+print("\nDid you know on the moon you would weight only %.1f pounds?" %float(float(weigh) / 6))
+print("But on the sun, you'd weigh %.1f (but, ah... not for long).\n\n" %float(float(weigh) *27.1))
+print("Press the enter key to exit.")
+
+
+
